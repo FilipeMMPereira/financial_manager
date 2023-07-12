0797117233f3540d5e3ad628520256b820363da4
Filipe Pereira <Filipe.Pereira@vm.co.mz>
2023-07-12 10:17:15 +0200

    First Commit

---
+++ app/src/main/java/com/example/financialmanager/LoginFragment.kt
@@ -0,0 +1,51 @@
+package com.example.financialmanager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.example.financialmanager.databinding.FragmentLoginBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding;
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return  binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonLogin.setOnClickListener {
+            val email = binding.editTextEmail.text.toString()
+            val password = binding.editTextPassword.text.toString()
+
+            if(email == "filipe@gmail.com" && password == "123456") {
+                Navigation.findNavController(view).navigate(R.id.homeFragment)
+            } else {
+                Toast.makeText(requireContext(), "Credenciais inválidas", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+}
